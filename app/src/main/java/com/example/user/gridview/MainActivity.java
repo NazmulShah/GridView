b6240ed374a5a0a4a1209980aea473e137e1c68c
Nazmul Shah <shahnazmul2k10@gmail.com>
2015-12-07 12:09:06 +0600

    slide show

--- app/src/main/java/com/example/user/gridview/MainActivity.java
+++ app/src/main/java/com/example/user/gridview/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.user.gridview;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 public class MainActivity extends Activity {
@@ -12,6 +15,18 @@ public class MainActivity extends Activity {
 
         GridView gridview = (GridView) findViewById(R.id.gridview);
         gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+
+                Intent i = new Intent(getApplicationContext(),ImageSwitcherExampleActivity.class);
+
+                // Pass image index
+                i.putExtra("id", position);
+
+                startActivity(i);
+            }
+        });
     }
 
 }
